c05e637e5dbb8dccedca4104dfb9f07dea955c9e
Noor <zcbmq14@gmail.com>
2026-02-06 19:47:39 +0300

    no coded word allowed

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-import { Telegraf } from "telegraf";
-import express from "express";
+const express = require("express");
+const { Telegraf } = require("telegraf");
+const { stripSmall, stripTatweel } = require("arajs");
+// import blockedWordsData from "./BlokckedWords.json" with { type: "json" };
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
-
+const bot = new Telegraf(process.env.BOT_TOKEN);
 const port = process.env.PORT;
+
+// const text = "السَّلَامُ عَلَيْكُمْ وَرَحْمَةُ اللَّهِ وَبَرَكَاتُهُ";
+// const text2 = "الســـلام علـــيكم ورحـــمة الله وبركاته";
+
+const TASHKEEL = /[\u0610-\u061A\u064B-\u065F\u0670\u06D6-\u06ED]/g;
+const PUNCTUATION = /[.,$…!?؛،:"'()\[\]{}<>@#%^&*_+=\\\/|-]/g;
+
+const normalize = (text = "") => {
+  return stripSmall(
+    stripTatweel(
+      text.replace(TASHKEEL, "").replace(PUNCTUATION, " ").replace(/\s+/g, " "),
+    ),
+  )
+    .toLowerCase()
+    .trim();
+};
 
 bot.start((ctx) => ctx.reply("Welcome to UPM Admin Bot!"));
 bot.hears("Hi", (ctx) => ctx.reply("Hi"));
-
-bot.on("text", (ctx) => {
-  const blockedWords = require("./BlokckedWords.json").blockedWords;
-  const messageText = ctx.message.text.toLowerCase();
-
-  for (const word of blockedWords) {
-    if (messageText.includes(word)) {
-      ctx.deleteMessage();
-      return;
-    }
-  }
+bot.on("text", async (ctx) => {
+  ctx.reply(normalize(ctx.message.text));
 });
 
+/*
+  bot.on("text", async (ctx) => {
+    const blockedWords = blockedWordsData.blockedWords;
+    const messageText = ctx.message.text.toLowerCase();
+
+    for (const word of blockedWords) {
+      if (messageText.includes(word.toLowerCase())) {
+        await ctx.deleteMessage();
+        return;
+      }
+    }
+  });
+*/
 bot.launch();
 
 app.listen(port, () => {
-  console.log(`bot now is running: http://localhost:${port}`);
+  console.log(`Bot is running on http://localhost:${port}`);
 });
 
 app.get("/", (req, res) => {
