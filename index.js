15068746798a72851d7d5c05e93d15a618c4a3d1
Noor <zcbmq14@gmail.com>
2026-02-08 21:28:27 +0300

    delete coded message

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const { Telegraf } = require("telegraf");
 const { stripSmall, stripTatweel } = require("arajs");
-// import blockedWordsData from "./BlokckedWords.json" with { type: "json" };
+const blockedWordsData = require("./blockedWords.json");
 
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const port = process.env.PORT;
 
-const TASHKEEL = /[\u0610-\u061A\u064B-\u065F\u0670\u06D6-\u06ED]/g;
-const PUNCTUATION = /[.,$…!?؛،:"'()\[\]{}<>@#%^&*_+=\\\/|-]/g;
+const tashkeel = /[\u0610-\u061A\u064B-\u065F\u0670\u06D6-\u06ED]/g;
+const punctuation = /[.,$…!?؛،:"'()\[\]{}<>@#%^&*_+=\\\/|-]/g;
 
 const normalize = (text = "") => {
   return stripSmall(
     stripTatweel(
-      text.replace(TASHKEEL, "").replace(PUNCTUATION, " ").replace(/\s+/g, " "),
+      text.replace(tashkeel, "").replace(punctuation, " ").replace(/\s+/g, " "),
     ),
   )
     .toLowerCase()
@@ -22,23 +22,22 @@ const normalize = (text = "") => {
 
 bot.start((ctx) => ctx.reply("Welcome to UPM Admin Bot!"));
 bot.hears("Hi", (ctx) => ctx.reply("Hi"));
+
 bot.on("text", async (ctx) => {
-  ctx.reply(normalize(ctx.message.text));
+  const blockedWords = blockedWordsData.blockedWords;
+  const messageText = ctx.message.text.toLowerCase();
+  const normalizedText = normalize(messageText);
+
+  for (const word of blockedWords) {
+    if (normalizedText.includes(word.toLowerCase())) {
+      await ctx.reply(
+        "Your message contains inappropriate content and has been removed.",
+      );
+      return;
+    }
+  }
 });
 
-/*
-  bot.on("text", async (ctx) => {
-    const blockedWords = blockedWordsData.blockedWords;
-    const messageText = ctx.message.text.toLowerCase();
-
-    for (const word of blockedWords) {
-      if (messageText.includes(word.toLowerCase())) {
-        await ctx.deleteMessage();
-        return;
-      }
-    }
-  });
-*/
 bot.launch();
 
 app.listen(port, () => {
